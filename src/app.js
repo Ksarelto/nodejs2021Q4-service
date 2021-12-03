9c7ddab7665fe4b3dc58746cb0c85f7dbe21e783
Artsiom Murashko <ksarelto@tut.by>
2021-12-03 09:57:19 +0300

    feat: change app file

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const swaggerUI = require('swagger-ui-express');
-const path = require('path');
-const YAML = require('yamljs');
-const userRouter = require('./resources/users/user.router');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const { userRouter } = require('./resources/users/user.router');
+const { boardRouter } = require('./resources/boards/boards.router');
+const { tasksRouter } = require('./resources/tasks/tasks.router');
 
-const app = express();
-const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+const app = new Koa();
 
-app.use(express.json());
+app.use(koaBody());
 
-app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
-app.use('/', (req, res, next) => {
-  if (req.originalUrl === '/') {
-    res.send('Service is running!');
+app.use(async (ctx, next) => {
+  if (ctx.originalUrl === '/') {
+    ctx.body('Service is running!');
     return;
   }
   next();
 });
 
-app.use('/users', userRouter);
+app.use(userRouter.routes()).use(userRouter.allowedMethods());
+app.use(boardRouter.routes()).use(boardRouter.allowedMethods());
+app.use(tasksRouter.routes()).use(tasksRouter.allowedMethods());
 
 module.exports = app;

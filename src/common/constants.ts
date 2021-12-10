5e75d3bb4ea00b029fffefb895e5be6a2781a807
Artsiom Murashko <ksarelto@tut.by>
2021-12-10 22:38:11 +0300

    refactor: update constants file

---
+++ src/common/constants.ts
@@ -0,0 +1,27 @@
+/**
+ * @module constants
+ */
+
+/**
+ * The list of status codes of responses
+ * @name codes
+ * @enum
+ */
+export enum StatusCodes {
+  notFound = 404,
+  invalidId = 400,
+  internalError = 500,
+  successCode = 200,
+  successDelete = 204,
+  successCreate = 201,
+};
+
+/**
+ * The list of response headers
+ * @constant 
+ * @type {object}
+ */
+export const headers = {
+  'Content-Type': 'application/json',
+};
+

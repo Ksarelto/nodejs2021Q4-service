e3d5df2f8afaacd95eda5fea6384e5ca3d4997be
Artsiom Murashko <ksarelto@tut.by>
2021-12-10 22:37:08 +0300

    refactor: update db file

---
+++ db/db.ts
@@ -0,0 +1,16 @@
+/**
+ * @module data_base
+ */
+
+import { DataBase } from "../src/common/types";
+
+/**
+ * This is a local DataBase object
+ * @constant DataBase
+ * @type {object}
+ */
+export const db: DataBase = {
+  users: [],
+  boards: [],
+  tasks: []
+}

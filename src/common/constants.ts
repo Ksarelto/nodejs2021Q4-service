a8abaef13665d8b1ca9e07961ef1f9de4225f9a3
Artsiom Murashko <ksarelto@tut.by>
2021-12-11 15:21:46 +0300

    refactor: update enum to object

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -7,13 +7,13 @@
  * @name codes
  * @enum
  */
-export enum StatusCodes {
-  notFound = 404,
-  invalidId = 400,
-  internalError = 500,
-  successCode = 200,
-  successDelete = 204,
-  successCreate = 201,
+export const StatusCodes = {
+  notFound: 404,
+  invalidId: 400,
+  internalError: 500,
+  successCode: 200,
+  successDelete: 204,
+  successCreate: 201,
 };
 
 /**

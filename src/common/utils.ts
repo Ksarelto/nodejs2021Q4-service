a5dc6fea57fcc433682ea732f17b591be0c5bc85
Artsiom Murashko <ksarelto@tut.by>
2021-12-19 14:48:22 +0300

    refactor: upddate tsdocs in utils.ts

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -6,23 +6,23 @@ import { User, SearchedArray } from "./types";
 import { headers } from './constants';
 
 /**
- * This function remove field password from User object
- * @param user - Is a User object
+ * This function remove field password from User object and return new User object
+ * @param {User} user - Is a User object
  * @returns The User without field password
  */
 
-export const toResponse = (user: User) => {
+export const toResponse = (user: User): Omit<User,'password'> => {
   const { name, login, id } = user;
   return { name, login, id };
 };
 
 /**
  * This function validate id
- * @param id - Is an id parametr of Item
+ * @param {string | undefined} id - Is an id parametr of Item
  * @returns If id valid return true, else false
  */
 
-export const validateID = (id: string | undefined) => {
+export const validateID = (id: string | undefined): boolean => {
   if (id === undefined) return false;
   if (!id.match(/^[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}$/g))
     return false;
@@ -31,14 +31,14 @@ export const validateID = (id: string | undefined) => {
 
 /**
  * This function check the existance ofsome Item in Array
- * @param arr - Array of Items
- * @param id - Id of searched item in array
- * @param name - Nmae of the Array of the Items
+ * @param {T} arr - Array of Items
+ * @param {string | undefined} id - Id of searched item in array
+ * @param {string} name - Nmae of the Array of the Items
  * @throws Error message if searchedItem is not find
- * @returns Searched Item or throw Error if item is undefined
+ * @returns Searched Item
  */
 
-export const checkExistence = <T extends SearchedArray>(arr: T[], id: string | undefined, name: string) => {
+export const checkExistence = <T extends SearchedArray>(arr: T[], id: string | undefined, name: string): SearchedArray => {
   if(!id) throw new Error(`Id is not defined`);
   const searchedItem = arr.find((item) => item.id === id);
   if (!searchedItem) throw new Error(`${name} with such id is not defined`);
@@ -47,9 +47,10 @@ export const checkExistence = <T extends SearchedArray>(arr: T[], id: string | u
 
 /**
  * This function send success response with data to client
- * @param context - Is an object that include request and response of server
- * @param data - Is some data that we should send to ckient
- * @param code - Code number of response
+ * @param {Context} context - Is an object that include request and response of server
+ * @param {T} data - Is some data that we should send to ckient
+ * @param {number} code - Code number of response
+ * @returns undefined 
  */
 
 export const successResponse = <T>(context: Context, data: T, code: number) => {
@@ -59,9 +60,10 @@ export const successResponse = <T>(context: Context, data: T, code: number) => {
 
 /**
  * This function send error response with error message to client
- * @param context - Is an object that include request and response of server
- * @param error - Is Error object
- * @param code - ode number of response
+ * @param {Context} context - Is an object that include request and response of server
+ * @param {unknown} error - Is Error object
+ * @param {number} code - ode number of response
+ * @returns undefined
  */
 
 export const errorResponse = (context: Context, error: unknown, code: number) => {

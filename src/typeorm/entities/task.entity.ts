d1e2b79022c05bba1f3fdf9e49bacb519ebc75ad
Artsiom Murashko <ksarelto@tut.by>
2022-01-11 01:11:08 +0300

    feat: create Task entities

---
+++ src/typeorm/entities/task.entity.ts
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-cycle */
+
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { EntBoard } from './board.entity';
+import { EntUser } from './user.entity';
+
+@Entity()
+export class EntTask {
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  order!: number;
+
+  @Column()
+  description!: string;
+
+  @Column({ type: 'uuid', nullable: true, name: 'column_id' })
+  columnId!: string | null;
+
+  @ManyToOne(() => EntBoard, (board) => board.tasks, {
+    eager: true,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  boardId!: EntBoard;
+
+  @ManyToOne(() => EntUser, (user) => user.tasks, {
+    eager: true,
+    nullable: true,
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
+  })
+  userId!: EntUser | null;
+}

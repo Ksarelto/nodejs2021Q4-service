c9d22fdb7b1f8dfffea2e692c4870762f3e21e9c
Artsiom Murashko <ksarelto@tut.by>
2022-01-23 19:34:28 +0300

    fix: add check user existing

--- src/resources/login/login.service.ts
+++ src/resources/login/login.service.ts
@@ -8,19 +8,29 @@ import {
 } from '../../common/errors.object';
 import { StatusCodes } from '../../common/constants';
 import { ILoginBody } from '../../common/types';
-import { generateToken } from '../../auth/access.token';
+import { generateToken } from '../../auth/generate.token';
 
 export const checkRegistration = async (body: ILoginBody) => {
   const { login, password } = body;
-  const searchedUser = await getRepository(EntUser).findOneOrFail({
+  const searchedUser = await getRepository(EntUser).findOne({
     login,
   });
-  const validatePassword = bcrypt.compareSync(password, searchedUser.password);
-  if (!validatePassword)
-    throw new CustomErrors(
-      errorNames.VE,
-      StatusCodes.invalidPassword,
-      errorMessages.invalidPass
+  if (searchedUser) {
+    const validatePassword = bcrypt.compareSync(
+      password,
+      searchedUser.password
     );
-  return generateToken({ id: searchedUser.id, login: searchedUser.login });
+    if (!validatePassword)
+      throw new CustomErrors(
+        errorNames.VE,
+        StatusCodes.invalidPassword,
+        errorMessages.invalidPass
+      );
+    return generateToken({ id: searchedUser.id, login: searchedUser.login });
+  }
+  throw new CustomErrors(
+    errorNames.NFE,
+    StatusCodes.invalidPassword,
+    errorMessages.noUser
+  );
 };

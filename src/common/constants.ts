939ede41a366c02619bdd3f5346769a203288328
Artsiom Murashko <ksarelto@tut.by>
2022-02-05 15:09:25 +0300

    refactor: update constatnts.ts

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,55 +1,22 @@
-/**
- * @module constants
- */
-
-/**
- * The list of status codes of responses
- * @constant Status_Codes
- * @enum
- */
-export const StatusCodes = {
-  notFound: 404,
-  invalidId: 400,
-  internalError: 500,
-  successCode: 200,
-  successDelete: 204,
-  successCreate: 201,
-  invalidPassword: 403,
-  invalidToken: 401,
-};
-
-/**
- * The list of response headers
- * @constant headers
- * @type {object}
- */
 export const headers = {
   'Content-Type': 'application/json',
 };
 
-/**
- * The list of log levels
- * @constant log_levels
- * @type {object}
- */
+export const passwordStrength = 7;
 
 export const LogLevelsObject = {
   crit: 0,
   error: 1,
   warn: 2,
-  http: 3,
+  info: 3,
+  http: 4,
 };
-
-/**
- * The list of colors for logs text
- * @constant log_colors
- * @type {object}
- */
 
 export const LogColorsObject = {
   crit: 'white redBG',
   error: 'red',
   warn: 'yellow',
+  info: 'green',
   http: 'magenta',
 };
 
@@ -58,3 +25,6 @@ export const defaultAdmin = {
   login: 'admin',
   password: 'admin',
 };
+
+export const databaseType = 'postgres';
+export const downloadFileDestination = './src/routes/files/db/';

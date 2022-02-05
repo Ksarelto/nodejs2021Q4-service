e56bdabd0680b5e41e19c99c11e08ee445c9dcdd
Artsiom Murashko <ksarelto@tut.by>
2022-02-05 14:55:42 +0300

    feat: add error handler

---
+++ src/exeptions/errors.object.ts
@@ -0,0 +1,16 @@
+export const requestedObjects = {
+  task: 'Task',
+  board: 'Board',
+  user: 'User',
+};
+
+export const errMess = {
+  invalid: 'Invalid id of ',
+  notExist: ' is not exist',
+  invalidPass: 'Invalid user password',
+  errorToken: 'JWT token was not set',
+  emptyToken: 'There is no token in request headers!',
+  invalidToken: 'This token is invalid',
+  existUser: 'User with such login has already been exist',
+  errDownload: 'Error during downloading',
+};
